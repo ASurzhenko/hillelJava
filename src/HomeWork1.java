8120f1a3aef23d8ca8b370b33aeeade43aae0248
Александр <britishop@gmail.com>
2016-02-13 01:27:29 +0200

    HomeWork 13.02.16

--- src/HomeWork1.java
+++ src/HomeWork1.java
@@ -3,20 +3,25 @@
  */
 public class HomeWork1 {
     public static void main(String[] args) {
-        int v1 = 10;
-        int v2 = 2;
-        int v3 = 2;
-        int min;
+        int v1 = 100;
+        int v2 = 10;
+        int v3 = 10;
 
-        if(v1 < v2 && v1 < v3){
-            min = v1;
+        if (v1 <= v2 && v1 <= v3) {
+            System.out.println(v1);
+        } else if (v2 <= v1 && v2 <= v3) {
+            System.out.println(v2);
+        } else {
+            System.out.println(v3);
         }
-        else if(v2 < v1 && v2 < v3){
-            min = v2;
+
+        if (v1 == v2 && v1 <= v3) {
+            System.out.println(v2);
+        } else if (v2 == v3 && v2 <= v3) {
+            System.out.println(v3);
+        } else if (v1 == v3 && v1 <= v2) {
+            System.out.println(v3);
         }
-        else{
-            min = v3;
-        }
-        System.out.println(min);
     }
 }
+
