0dce3d6eb8b97f2b9cc1a8084bef7df77dd66925
Александр <britishop@gmail.com>
2016-02-15 20:08:11 +0200

    HomeWork 13.02.16

--- src/HomeWork1.java
+++ src/HomeWork1.java
@@ -1,27 +1,23 @@
 /**
  * Created by Александр on 13.02.2016.
  */
+
+/**
+ * Найти минимальное из 3х чисел, причем,
+ * если минимальных несколько вывести все такие числа
+ */
 public class HomeWork1 {
     public static void main(String[] args) {
         int v1 = 100;
         int v2 = 10;
         int v3 = 10;
 
-        if (v1 <= v2 && v1 <= v3) {
+        if (v1 <= v2 && v1 <= v3)
             System.out.println(v1);
-        } else if (v2 <= v1 && v2 <= v3) {
+        if (v2 <= v1 && v2 <= v3)
             System.out.println(v2);
-        } else {
+        if (v3 <= v1 && v3 <= v2)
             System.out.println(v3);
-        }
-
-        if (v1 == v2 && v1 <= v3) {
-            System.out.println(v2);
-        } else if (v2 == v3 && v2 <= v3) {
-            System.out.println(v3);
-        } else if (v1 == v3 && v1 <= v2) {
-            System.out.println(v3);
-        }
     }
 }
 
